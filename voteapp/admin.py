1e9dd3d909045299d11ddec29606aaf00b802c3e
susmitha <pillisusmitha097@gmail.com>
2023-09-25 16:21:08 +0530

    more changes

--- voteapp/admin.py
+++ voteapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Category, CategoryItem
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(CategoryItem)
+
+
